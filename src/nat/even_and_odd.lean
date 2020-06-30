d3a5547d5a27d8e0fc6d6283ef7f028aa70d39e1
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-06-30 09:31:03 +0100

    add even and odd

---
+++ src/nat/even_and_odd.lean
@@ -0,0 +1,55 @@
+def even (n : ℕ) : Prop := ∃ d, n = 2 * d
+def odd (n : ℕ) : Prop := ∃ d, n = 2 * d + 1
+
+/-
+see https://www.codewars.com/kata/5e998b42dcf07b0001581def/lean
+for a completely different approach (inductive definitions)
+-/
+
+/- ## interaction with 0 -/
+
+lemma even_zero : even 0 := sorry
+
+/- ## interaction with succ -/
+
+lemma even_add_one {n : ℕ} : even n → odd (n + 1) := sorry
+
+lemma odd_add_one {n : ℕ} : odd n → even (n + 1) := sorry
+
+/- ## interaction with add -/
+
+theorem odd_add_odd {n m : ℕ} (hn : odd n) (hm : odd m) :
+  even (n + m) := sorry
+
+theorem odd_add_even {n m : ℕ} (hn : odd n) (hm : even m) :
+  odd (n + m) := sorry
+
+theorem even_add_odd {n m : ℕ} (hn : even n) (hm : odd m) :
+  odd (n + m) := sorry
+
+theorem even_add_even {n m : ℕ} (hn : even n) (hm : even m) :
+  even (n + m) := sorry
+
+/- ## interaction with one -/
+
+theorem odd_one : odd 1 := sorry
+
+/- ## interaction with mul -/
+
+theorem even_mul_even {n m : ℕ} (hn : even n) (hm : even m) :
+  even (n * m) := sorry
+
+theorem even_mul_odd {n m : ℕ} (hn : even n) (hm : odd m) :
+  even (n * m) := sorry
+
+theorem odd_mul_even {n m : ℕ} (hn : odd n) (hm : even m) :
+  even (n * m) := sorry
+
+theorem odd_mul_odd {n m : ℕ} (hn : odd n) (hm : odd m) :
+  odd (n * m) := sorry
+
+/- ## interaction with each other -/
+
+lemma not_odd_and_even {n : ℕ} : ¬ (odd n ∧ even n) := sorry
+
+lemma odd_or_even (n : ℕ) : odd n ∨ even n := sorry 

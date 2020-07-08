b8b2fa4ee301c9918ff637deaa78b357f5629629
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-07-08 17:14:44 +0100

    tidy

--- src/elliptic_curves/basic.lean
+++ src/elliptic_curves/basic.lean
@@ -1,7 +1,7 @@
 import tactic
 
 open_locale classical -- assume the law of the excluded middle
-noncomputable theory -- 
+noncomputable theory -- because Lean can't calculate with an arbitrary field
 
 /-- An elliptic curve over a field -/
 structure elliptic_curve (k : Type) [field k] :=
@@ -18,7 +18,6 @@ def points (E : elliptic_curve k) := with_zero {P : k × k // let ⟨x, y⟩ := 
   y^2 + E.a1*x*y + E.a3*y = x^3 + E.a2*x^2 + E.a4*x + E.a6}
 
 -- "let E be an elliptic curve over k"
-
 variable (E : elliptic_curve k)
 
 /-- notation 0 for the "extra point" we added -/
